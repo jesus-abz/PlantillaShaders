22c8bae607e5167fca5dd30b4a008483ea10d1d7
jesus.abarron.z <jesus.abarron.z@gmail.com>
2020-03-02 07:48:41 -0700

    en el paso 7

--- PlantillaGraficos/Shader.cpp
+++ PlantillaGraficos/Shader.cpp
@@ -51,7 +51,17 @@ Shader::Shader(const char* rutaVertexShader, const char* rutaFragmentShader) {
 	glCompileShader(fragmentShaderID);
 
 	//4. Verificar Errores de Compilacion
+	verificarCompilacion(vertexShaderID);
+	verificarCompilacion(fragmentShaderID);
 
+	//5. Ajustar los shaders al programa
+	glAttachShader(shaderID, vertexShaderID);
+	glAttachShader(shaderID, fragmentShaderID);
+
+	//6. Vincular el programa
+	glLinkProgram(shaderID);
+
+	//7. Verificar si hubo errores en la vinculacion
 }
 
 void Shader::verificarCompilacion(GLuint id) {
@@ -60,4 +70,18 @@ void Shader::verificarCompilacion(GLuint id) {
 
 	glGetShaderiv(id, GL_COMPILE_STATUS, &resultado);
 	glGetShaderiv(id, GL_INFO_LOG_LENGTH, &longitudLog);
+
+	if (resultado == GL_FALSE) {
+		cout << "No se pudo compilar shader" << endl;
+	}
+	if (longitudLog > 0) {
+		//Inicia una lista de char con el numero de elementos indicados en longitudLog
+		vector<char> mensajeError(longitudLog);
+		//obtener mensajes del compilador y los guarda en la variable mensajeError
+		glGetShaderInfoLog(id, longitudLog, NULL, &mensajeError[0]);
+		//recorrer el vector e imprimir sus elementos
+		for (vector<char>::const_iterator i = mensajeError.begin(); i != mensajeError.end(); i++) {
+			cout << *i;
+		}
+	}
 }
