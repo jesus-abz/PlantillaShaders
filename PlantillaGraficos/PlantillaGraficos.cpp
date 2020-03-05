9375904f8cb25aa5296c87bc66ddd0a05ab2959a
jesus.abarron.z <jesus.abarron.z@gmail.com>
2020-03-05 07:48:54 -0700

    inicializacion shader

--- PlantillaGraficos/PlantillaGraficos.cpp
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -11,7 +11,37 @@
 
 #include <iostream>
 
+#include "Shader.h"
+#include "Vertice.h"
+#include <vector>
+
 using namespace std;
+
+//Cada elemento que queramos renderear necesita un vertex array y un buffer
+vector <Vertice> triangulo;
+GLuint vertexArrayTriangulo;
+GLuint bufferTrianguloID;
+
+//Instancia del shader
+Shader *shader;
+//Identificadores para mapeo de atributos de entrada del vertex shader
+GLuint posicionID;
+GLuint colorID;
+
+void inicializarTriangulo() {
+    Vertice v1 = {
+        vec3(0.0f,0.3f,0.0f), vec4(0.8f,0.1f,0.0f,1.0f)
+    };
+    Vertice v2 = {
+        vec3(-0.3f,-0.3f,0.0f), vec4(0.8f,0.1f,0.0f,1.0f)
+    };
+    Vertice v3 = {
+        vec3(0.3f,-0.3f,0.0f), vec4(0.8f,0.1f,0.0f,1.0f)
+    };
+    triangulo.push_back(v1);
+    triangulo.push_back(v2);
+    triangulo.push_back(v3);
+}
 
 void dibujar() {
 }
@@ -47,6 +77,12 @@ int main()
 
     const GLubyte* versionGL = glGetString(GL_VERSION);
     cout << "Version OpenGL: " << versionGL;
+
+    inicializarTriangulo();
+
+    const char* rutaVertexShader = "VertexShader.shader";
+    const char* rutaFragmentShader = "FragmentShader.shader";
+    shader = new Shader(rutaVertexShader, rutaFragmentShader);
 
     //ciclo de dibujo (Draw Loop)
     while (!glfwWindowShouldClose(window)) {
--- PlantillaGraficos/Vertice.h
+++ PlantillaGraficos/Vertice.h
@@ -6,8 +6,8 @@ struct vec3 {
 };
 
 struct vec4 {
-	vec4(float _x = 0.0f, float _y = 0.0f, float _z = 0.0f, float _w = 0.0f) : x(_x), y(_y), z(_z), w(_w) {}
-	float x, y, z, w;
+	vec4(float _r = 0.0f, float _g = 0.0f, float _b = 0.0f, float _w = 0.0f) : r(_r), g(_g), b(_b), w(_w) {}
+	float r, g, b, w;
 };
 
 struct Vertice
